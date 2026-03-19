41e7a4e520697e6b583cf89f07cea6c8ae7d7160
Awais Rajpoot <154225077+awais182@users.noreply.github.com>
2026-03-19 12:44:12 +0500

    ok

--- ExpertiseGrid.tsx
+++ ExpertiseGrid.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { Icons } from '../../constants';
+import { Icons } from '../constants';
 
 const ExpertiseGrid: React.FC = () => {
   const expertises = [
